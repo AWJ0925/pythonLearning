3c56574eed5579f5cd56a37fa1f2313793ac701c
AWJ0925 <63305752+AWJ0925@users.noreply.github.com>
2021-12-29 20:52:42 +0800

    Add files via upload

--- 00_变量/varTest01.py
+++ 00_变量/varTest01.py
@@ -1,8 +1,8 @@
 # 1.变量三要素：变量名、变量类型、变量的值
 name = "玛丽亚"
-print('id：', id(name))
-print('type：', type(name))
-print('value：', name)
+print('id：', id(name))  # 变量的内存地址
+print('type：', type(name))  # 变量值的数据类型
+print('value：', name)  # 变量的值
 
 # 2.变量的操作：访问、修改值
 name = "玛丽亚"
@@ -13,7 +13,7 @@ print(name, id(name))
 # 函数的使用
 age = 18
 name = "yuHuan"
-# 判断地址是否一样
+# var_1 is var_2：判断地址是否一样
 print(age is name)  # False
-# 判断值相等？
+# var_1 == var_2：判断值相等？
 print(age == name)  # False
